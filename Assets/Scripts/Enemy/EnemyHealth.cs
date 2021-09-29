0c9f0bb25178c86100c78b71180defe5b8b65c2c
Skicom123 <fajarrahmanarajab@gmail.com>
2021-09-29 23:31:57 +0700

    -

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
+    public AudioClip deathClip;
+
+
+    Animator anim;
+    AudioSource enemyAudio;
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake ()
+    {
+        //mendapatkan referensi komponen
+        anim = GetComponent <Animator> ();
+        enemyAudio = GetComponent <AudioSource> ();
+        hitParticles = GetComponentInChildren <ParticleSystem> ();
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update ()
+    {
+        if (isSinking)
+        {
+            //memindahkan object ke bawah
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        if (isDead)
+            return;
+
+        enemyAudio.Play ();
+
+        currentHealth -= amount;
+
+        //ganti posisi particle serta memPlay
+        hitParticles.transform.position = hitPoint;
+        hitParticles.Play();
+
+        if (currentHealth <= 0)
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        //set isdead
+        isDead = true;
+ 
+        //SetCapcollider ke trigger
+        capsuleCollider.isTrigger = true;
+ 
+        //trigger play animation Dead
+        anim.SetTrigger ("Dead");
+ 
+        //Play Sound Dead
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play ();
+    }
+
+
+    public void StartSinking ()
+    {
+        //disable Navmesh Component
+        GetComponent <UnityEngine.AI.NavMeshAgent> ().enabled = false;
+        //Set rigisbody ke kimematic
+        GetComponent <Rigidbody> ().isKinematic = true;
+        isSinking = true;
+        ScoreManager.score += scoreValue;
+        Destroy (gameObject, 2f);
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    private void Awake ()
+    {
+        //cari game object dengan player tag
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+        //Mendapatkan Reference component
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        
+    }
+
+
+    void Update ()
+    {
+        //Memindahkan posisi player
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth> 0)
+        {
+           nav.SetDestination(player.position);
+        }
+        //Hentikan moving
+        else 
+        {
+            nav.enabled = false;
+        }
+        
+    }
+}
